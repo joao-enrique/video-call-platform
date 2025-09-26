07edca21e4a39de33f76a10f46e173464372d401
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-09-26 12:17:08 -0300

    💾 Feat: Connecting convex with clerk utilizing webhooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-10">
-      Home page
+      <SignInButton>
+        <Button>Login</Button>
+      </SignInButton>
     </div>
   );
 }
