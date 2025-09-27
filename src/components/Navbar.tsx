8dd0d3a37efa7028b1b147b2c6bb7c2e474fc103
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-09-27 17:33:42 -0300

    💾 feat: add dropdown menu for theme toggle and update navbar
    - Added @radix-ui/react-dropdown-menu dependency for dropdown functionality.
    - Implemented ModeToggle component to allow users to switch between light, dark, and system themes.
    - Updated Navbar component to include a logo and improved layout.
    - Modified RootLayout to suppress hydration warnings.
    - Simplified Home page by removing SignInButton and adding a placeholder text.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { ModeToggle } from './ModeToggle'
+import Link from 'next/link'
+import { PhoneCall } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='border-b'>
+        <div className='flex h-16 items-center px-4 conatiner mx-auto'>
+            {/* left side -LOGO */}
+            <Link href="/" className='flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80'>
+                <PhoneCall className='size-8 text-emerald-500'/>
+                <span className='bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent'>
+                    HireCall
+                </span>
+            </Link>
+        </div>
+    </nav>
   )
 }
 
