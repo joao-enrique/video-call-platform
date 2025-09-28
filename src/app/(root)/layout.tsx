03374efa6cf158fd967b685fd023c8d48a1ef533
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-09-28 11:36:28 -0300

    💾 feat: implement StreamVideoProvider with token generation and loading state

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import StreamClientProvider from "@/components/Providers/StreamClientProvider"
+
+function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <StreamClientProvider>{children}</StreamClientProvider>
+  )
+}
+
+export default Layout
